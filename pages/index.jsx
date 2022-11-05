fe5972fe0522be5d4fa23f028b1010ec81189224
Fran Markulin <fran.markulin@fer.hr>
2022-11-05 12:00:31 +0100

    added signout for testing

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,18 @@
+import { useEffect } from 'react';
 import Layout from '../components/layout'
 import { useAuth } from '../lib/context';
 
 export default function Home() {
-  const {authUser} = useAuth();
+  const {authUser, loading, firebaseSignOut } = useAuth();
+
   console.log("index: ", authUser);
   return (
     <Layout>
       <h1>hello</h1>
       <p>{authUser ? authUser.uid : "no user"}</p>
       <a href="/register">register</a>
+      {authUser && !loading && <button onClick={firebaseSignOut}>sign out</button>}
+
     </Layout>
   )
 }
