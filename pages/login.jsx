7994330b842765e10ca485d9d0d06369581ddc92
BranachM <branimir.medvedec@fer.hr>
2022-11-05 14:06:05 +0100

    Komentari na Login page
    Dodani komentari za login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -4,56 +4,56 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { useAuth } from "../lib/context";
 
+// funkcija za login sa emailom i passwordom koja prima parametre email i password u sklopu params objekta
 export default function Login(params) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const { firebaseEmailPassSignIn } = useAuth();
-    const router = useRouter();
+    // inicijalizacija hook state-a za email i password
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+    // inicijalizacija hook-a za autentifikaciju
+  const { firebaseEmailPassSignIn } = useAuth();
+  const router = useRouter();
 
-    function handleSubmit(event) {
-        event.preventDefault();
+  //funckija koja se izvrsava na pritisak gumba
+  function handleSubmit(event) {
+    event.preventDefault();
 
-        firebaseEmailPassSignIn(email, password)
-            .then((authUser) => {
-                //Signed in
-                router.push("/");
-                // ...
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // ..
-            });
-    }
+    // predajemo email i password custom hooku koji komunicira sa firebaseom
+    // ako je uspjesno autentificiranje, preusmjeri na pocetnu stranicu
+    firebaseEmailPassSignIn(email, password)
+      .then((authUser) => {
+        router.push("/");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
 
-    return (
-        <Layout page="Login">
-            <div className={styles.container}>
-                <form onSubmit={handleSubmit}>
-                    <div className="card">
-                        <h1>Login</h1>
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(event) => {
-                                setEmail(event.target.value);
-                            }}
-                        />
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(event) => {
-                                setPassword(event.target.value);
-                            }}
-                        />
-                        <input
-                            className="button"
-                            type="submit"
-                            value="Submit"
-                        />
-                    </div>
-                </form>
-            </div>
-        </Layout>
-    );
+  // prikaz forme za login
+  return (
+    <Layout page="Login">
+      <div className={styles.container}>
+        <form onSubmit={handleSubmit}>
+          <div className="card">
+            <h1>Login</h1>
+            <input
+              type="email"
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+            <input className="button" type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
+    </Layout>
+  );
 }
